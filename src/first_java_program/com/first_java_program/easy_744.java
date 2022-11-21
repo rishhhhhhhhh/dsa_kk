840ee8687b35fc81c3b74287b70b13816822526d
Risha Rahaman <risha2031@gmail.com>
2022-11-21 17:52:11 +0530

    easy lc

---
+++ src/first_java_program/com/first_java_program/easy_744.java
@@ -0,0 +1,25 @@
+package com.first_java_program;
+
+public class easy_744 {
+           public static void main(String[] args) {
+            
+           }
+    
+        public char nextGreatestLetter(char[] letters, char target) {
+            
+            int start=0;
+            int end=letters.length-1;
+           
+        
+            while(start<=end){
+                int mid = start + (end-start)/2;
+                if(target< letters[mid]){
+                    end = mid -1;
+                }
+                else {
+                    start = mid+1;
+                }
+            }
+            return letters[start%(letters.length)];
+        }
+    }
