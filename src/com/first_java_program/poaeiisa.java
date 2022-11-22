ad33ee71f658b9dc334c42041a0d99008562dac5
Risha Rahaman <risha2031@gmail.com>
2022-11-22 15:45:10 +0530

    updated

---
+++ src/com/first_java_program/poaeiisa.java
@@ -0,0 +1,42 @@
+package com.first_java_program;
+
+public class poaeiisa {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,34,67,78,90,678,56,45};
+        int target = 89;
+        System.out.println(ans(arr, target));
+    }
+
+    static int ans(int[] arr, int target) {
+       
+        int start = 0;
+        int end = 1;
+
+       
+        while (target > arr[end]) {
+            int newstart = end + 1; 
+            
+            end = end + (end - start + 1) * 2;
+            start = newstart;
+        }
+
+        return BinarySearch(arr, target, start, end);
+
+    }
+    static int BinarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+          
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
