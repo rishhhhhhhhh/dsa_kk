c6bd9e6903d6615fd6f7f5a4912386a1fe1b3921
Risha Rahaman <risha2031@gmail.com>
2022-11-22 20:54:47 +0530

    lc easy

---
+++ src/first_java_program/com/first_java_program/easy_1812.java
@@ -0,0 +1,15 @@
+package com.first_java_program;
+
+public class easy_1812 {
+    public static void main(String[] args) {
+        String n = "a1";
+        System.out.println(squareIsWhite(n));
+        
+    }
+  
+    public static boolean squareIsWhite(String coordinates) {
+        
+        return (int)coordinates.charAt(0)%2 != (int)coordinates.charAt(1)%2;
+       
+   }
+}
