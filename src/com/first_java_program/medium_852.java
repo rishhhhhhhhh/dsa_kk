18b5a2f77d05d348b67a28f85dc33d568b610d42
Risha Rahaman <risha2031@gmail.com>
2022-11-22 16:06:53 +0530

    lc medium

---
+++ src/com/first_java_program/medium_852.java
@@ -0,0 +1,28 @@
+package com.first_java_program;
+
+public class medium_852 {
+    public static void main(String[] args) {
+
+    }
+   
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (arr[mid] > arr[mid+1]) {
+               
+                end = mid;
+            } 
+            else {
+                
+                start = mid + 1; 
+            }
+        }
+        
+        return start; 
+    }
+}
+
