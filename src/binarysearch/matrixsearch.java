c381b8b125220d9275e204d0aab58cae22553a20
Risha Rahaman <risha2031@gmail.com>
2022-12-13 17:12:22 +0530

    updated

---
+++ src/binarysearch/matrixsearch.java
@@ -0,0 +1,34 @@
+package binarysearch;
+
+import java.util.Arrays;
+
+public class matrixsearch {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+
+        System.out.println(Arrays.toString(search(mat, 69)));
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix.length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return new int[]{row, col};
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            } 
+            else {
+                col--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
