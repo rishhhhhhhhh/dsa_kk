8097db443afe2695352bd4e842079afbf78fb9e9
Risha Rahaman <risha2031@gmail.com>
2022-12-14 19:19:19 +0530

    sorting

---
+++ src/sorting/bubblesort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class bubblesort {
+    public static void main(String[] args) {
+        int[] arr = {69, 57, 89, 33, 22};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        boolean swapped;
+    
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+         
+            for (int j = 0; j < arr.length - i - 1; j++) {   
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped==false) { 
+                break;
+            }
+        }
+    }
+}
