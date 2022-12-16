c8d9aeb30b3c35695d8e1475348f2dd69116cfa7
Risha Rahaman <risha2031@gmail.com>
2022-12-16 18:29:50 +0530

    updated

--- src/leetcode/easy_442.java
+++ src/leetcode/easy_442.java
@@ -31,3 +31,24 @@ public class easy_442 {
         arr[second] = temp;
     }
 }
+
+// public List<Integer> findDuplicates(int[] arr) {
+//     List<Integer> ans = new ArrayList<>();
+//     for(int i=0;i<arr.length;i++){
+//         while((i+1)!=arr[i]){
+//             if(arr[arr[i]-1] == arr[i]){
+//                 break;
+//             }
+//             int temp =arr[arr[i]-1];
+//             arr[arr[i]-1] = arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+
+//     for(int i=0;i<arr.length;i++){
+//         if(arr[i]!=i+1){
+//             ans.add(arr[i]);
+//         }
+//     }
+//     return ans;
+// }
