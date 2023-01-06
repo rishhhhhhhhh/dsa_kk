10425236abb4c94bb794a23dfe5d787c5414ee34
Risha Rahaman <risha2031@gmail.com>
2023-01-06 20:16:52 +0530

    maths

---
+++ src/mathsindsa/unique.java
@@ -0,0 +1,18 @@
+package mathsindsa;
+
+public class unique {
+    public static void main(String[] args) {
+            int[] arr = {1,2,3,4,5,4,3,2,1};
+            System.out.println(ans(arr));
+        }
+    
+        private static int ans(int[] arr) {
+            int unique = 0;    
+            for(int n : arr) {
+                unique ^= n;
+            }
+    
+            return unique;
+        }
+    }
+
