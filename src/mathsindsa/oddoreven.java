f7c9763f04333ccc3e87b084050382c4645f1607
Risha Rahaman <risha2031@gmail.com>
2023-01-06 20:10:48 +0530

    maths

---
+++ src/mathsindsa/oddoreven.java
@@ -0,0 +1,11 @@
+package mathsindsa;
+
+public class oddoreven {
+    public static void main(String[] args) {
+        System.out.println(isOdd(56));
+    }
+    
+    private static boolean isOdd(int n){
+        return(n&1)==1;
+    }
+}
