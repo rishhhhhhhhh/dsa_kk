e11239e8ccf47fea01b3b7633407b267d86c29f6
Risha Rahaman <risha2031@gmail.com>
2023-01-09 16:33:04 +0530

    maths

--- src/mathsindsa/findi.java
+++ src/mathsindsa/findi.java
@@ -2,7 +2,7 @@ package mathsindsa;
 
 public class findi {
     public static void main(String[] args) {
-        int num = 10011000;
+        int num = 10001000;
         System.out.println(num & (1<<4));
     }    
 }
---
+++ src/mathsindsa/magicnumber.java
@@ -0,0 +1,22 @@
+package mathsindsa;
+
+public class magicnumber {
+    public static void main(String[] args) {
+        int n=2;
+        int ans=0;
+        int base=5;
+
+        while(n>0){
+            int last=n&1;
+            n=n>>1;
+            ans= ans+ last*base;
+            base=base*5;        }
+        System.out.println(ans);
+    }
+
+    
+    
+
+    
+    
+}
