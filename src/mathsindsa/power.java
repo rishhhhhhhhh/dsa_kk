686e5978f6dfc3bf847cbed8d06937c303fb3fa3
Risha Rahaman <risha2031@gmail.com>
2023-01-09 17:18:08 +0530

    maths

---
+++ src/mathsindsa/power.java
@@ -0,0 +1,18 @@
+package mathsindsa;
+
+public class power {
+    public static void main(String[] args) {
+        int base = 5;
+        int power = 5;
+        int powerr = 1;
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                powerr *= base;
+            }
+            base *= base;
+            power = power >> 1;
+        }
+        System.out.println(powerr);
+    }
+}
