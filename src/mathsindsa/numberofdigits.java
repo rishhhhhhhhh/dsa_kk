23b1b3f71e0363eb070459e791160cb85d6676ef
Risha Rahaman <risha2031@gmail.com>
2023-01-09 16:47:54 +0530

    maths

---
+++ src/mathsindsa/numberofdigits.java
@@ -0,0 +1,15 @@
+package mathsindsa;
+
+
+public class numberofdigits {
+    public static void main(String[] args) {
+        int a=6;
+        int b=2;
+    
+        int count= (int)(Math.log(a)/Math.log(b)) +1;
+        System.out.println(count); 
+    }
+  
+
+    
+}
