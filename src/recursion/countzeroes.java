1ee8346b8adeb210084b4a79822e19921eeece17
Risha Rahaman <risha2031@gmail.com>
2023-01-21 14:38:41 +0530

    recursion

---
+++ src/recursion/countzeroes.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class countzeroes {
+
+    public static void main(String[] args) {
+        System.out.println(countz(1000));
+    }
+
+    static int countz(int n) {
+        return count(n, 0);
+    }
+
+    private static int count(int n, int c) {
+        if (n == 0) {
+            return c;
+        }
+
+        int rem = n % 10;
+        if (rem == 0) {
+            return count(n/10, c+1);
+        }
+        return count(n/10, c);
+    }
+    
+}
