8f7111b054e0da1df9560b215035bf5db8c6943b
Risha Rahaman <risha2031@gmail.com>
2023-02-01 17:08:37 +0530

    recursion

--- src/recursion/permutation.java
+++ src/recursion/permutation.java
@@ -1,5 +1,56 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class permutation {
-    
+    public static void main(String[] args) {
+       permutations("", "abcd");
+       ArrayList<String> ans = permutationsAL("", "abcd");
+      System.out.println(ans);
+       System.out.println(permutationscount("", "abcde"));
+    }
+
+    static void permutations(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0, i);
+            String second = p.substring(i, p.length());
+            permutations(first + ch + second, up.substring(1));            
+        }
+    }
+
+    static ArrayList<String> permutationsAL(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsAL(f + ch + s, up.substring(1)));
+        }
+        return ans;
+    }
+
+    static int permutationscount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count = count + permutationscount(f + ch + s, up.substring(1));
+        }
+        return count;
+    }
 }
