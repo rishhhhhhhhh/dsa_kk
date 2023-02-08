8f500add9592d19a5146316bffdebb5f0b86ffb8
Risha Rahaman <risha2031@gmail.com>
2023-02-08 19:52:28 +0530

    maze backtracking

--- src/recursion/backtracking/mazecount.java
+++ src/recursion/backtracking/mazecount.java
@@ -6,6 +6,8 @@ public class mazecount {
         System.out.println(count(3, 3));
         mazepath("", 3, 3);
         System.out.println(mazepathAL("", 3, 3));
+        System.out.println(mazepathdiag("", 3, 3));
+
     }
 
     static int count(int row, int column) {
@@ -42,6 +44,25 @@ public class mazecount {
         }
         if (row > 1) {
             list.addAll(mazepathAL(ans + 'D', row-1, column));
+        }
+        return list;
+    }
+
+    static ArrayList<String> mazepathdiag(String ans, int row, int column) {
+        if (row == 1 && column == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>(); 
+        if (row > 1 && column > 1) {
+            list.addAll(mazepathdiag(ans + 'I', row-1, column-1));
+        }       
+        if (column > 1) {
+            list.addAll(mazepathdiag(ans + 'R', row, column-1));
+        }
+        if (row > 1) {
+            list.addAll(mazepathdiag(ans + 'D', row-1, column));
         }
         return list;
     }
