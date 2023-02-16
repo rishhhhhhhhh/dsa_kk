c5216cf934f33780c1170781e3924b5324184bcf
Risha Rahaman <risha2031@gmail.com>
2023-02-16 19:10:04 +0530

    leetcode easy

---
+++ src/leetcode/leetcode/easy_104.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class easy_104 {
+    
+ //Definition for a binary tree node.
+ public class TreeNode {
+    int val;
+     TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+ }
+ 
+    public int maxDepth(TreeNode root) {
+    if (root == null) {
+    return 0; // base case: an empty tree has depth 0
+    } else {
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+    return 1 + Math.max(leftDepth, rightDepth); // recursive case: the depth is 1 plus the maximum depth of the left and right subtrees
+    }
+    }
+    }
+
