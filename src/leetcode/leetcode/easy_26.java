fd7cdd418f7795c28234e863b48726a5a4df354a
Risha Rahaman <risha2031@gmail.com>
2023-02-20 15:35:11 +0530

    leetcode easy

---
+++ src/leetcode/leetcode/easy_26.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class easy_26 {
+    public static void main(String[] args) {
+          
+    }
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }   
+        return i + 1;
+    }
+}
