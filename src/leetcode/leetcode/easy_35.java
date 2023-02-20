cedc68b4df895492dd2204b4b14bd8900a603783
Risha Rahaman <risha2031@gmail.com>
2023-02-20 13:45:29 +0530

    leetcode easy

--- src/leetcode/leetcode/easy_35.java
+++ src/leetcode/leetcode/easy_35.java
@@ -2,21 +2,18 @@ package leetcode;
 
 public class easy_35 {
    public static void main(String[] args) {
-      int[] arr = {1,3,4,5};
-      System.out.println(searchInsert(arr, 2)); 
+      int[] nums = {1,3,4,5};
+      System.out.println(searchInsert(nums, 5)); 
    }
 
-public static int searchInsert(int[] arr, int target) {
+public static int searchInsert(int[] nums, int target) {
        
-         for(int i = 0; i < arr.length; i++)
-            if(arr[i] == target) {
-               return i;
-            }
-            else if (arr[i] > target){
+   for(int i = 0; i < nums.length; i++){
+      if(nums[i] >= target) {
               return i;
-            } 
-
-           return arr.length;
+           }
+     }
+          return nums.length;
     }
    
    }
