07715858b26af46893e4800826589d13701436e5
Risha Rahaman <risha2031@gmail.com>
2023-02-24 19:17:39 +0530

    leetcode hard

---
+++ src/leetcode/hard/hard_1675.java
@@ -0,0 +1,34 @@
+package hard;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class hard_1675 {
+    public static void main(String[] args) {
+        
+    }
+
+        public int minimumDeviation(int[] nums) {
+            PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+            int minValue=Integer.MAX_VALUE;
+            for(int x:nums){
+                if((x&1)==1)
+                    x<<=1;
+                pq.add(x);
+                minValue=Math.min(minValue,x);
+            }
+            int minDeviation=Integer.MAX_VALUE;
+            while(!pq.isEmpty()){
+                int curr=pq.poll();
+                minDeviation=Math.min(minDeviation,curr-minValue);
+                if((curr&1)==1)
+                    break;
+                curr>>=1;
+                minValue=Math.min(minValue,curr);
+                pq.add(curr);
+            }
+            return minDeviation;
+        }
+    
+    
+}
