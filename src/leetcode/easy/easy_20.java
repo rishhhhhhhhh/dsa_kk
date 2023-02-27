bb1c8c4f6b7995df3d3bbff97746f331d52cfc84
Risha Rahaman <risha2031@gmail.com>
2023-02-27 20:40:10 +0530

    leetcode easy

---
+++ src/leetcode/easy/easy_20.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.ArrayList;
+
+public class easy_20 {
+    public static void main(String[] args) {
+        System.out.println(isValid("{]"));
+    }
+    public static boolean isValid(String s) {
+        ArrayList<Character> arr = new ArrayList<Character>();
+
+    for(int i = 0 ; i < s.length() ;i++){
+        if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[' ){
+            arr.add(s.charAt(i));
+        }
+        else if(s.charAt(i) == ')'){
+            if(arr.size()>0 && arr.get(arr.size()-1) == '('){
+                arr.remove(arr.size()-1);
+            }else{
+                return false ;
+            }
+        }else if(s.charAt(i) == '}'){
+            if(arr.size()>0 && arr.get(arr.size()-1) == '{'){
+                arr.remove(arr.size()-1);
+            }else{
+                return false ;
+            }
+        }else if(s.charAt(i) == ']'){
+            if(arr.size()>0 && arr.get(arr.size()-1) == '['){
+                arr.remove(arr.size()-1);
+            }else{
+                return false ;
+            }
+        }
+    }      
+        return (arr.size()==0);
+    }
+}
