a3bbcb49ecb9928aeca300901f9d0f163ba7e73f
Risha Rahaman <risha2031@gmail.com>
2023-03-02 20:12:09 +0530

    leetcode easy

---
+++ src/leetcode/easy/easy/easy_69.java
@@ -0,0 +1,25 @@
+package easy;
+public class easy_69 {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(25));
+    }
+        public static int mySqrt(int x) {
+            int left = 1;
+            int right = x;
+    
+            while(left <= right){
+                int mid = left + (right-left)/2;
+    
+                if(x / mid == mid){
+                    return mid;
+                } else if(mid > x / mid){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+    
+            return right;
+        }
+    
+}
