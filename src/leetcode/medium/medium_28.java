4e8513b003a010bfc939cb3f588c7338009ef232
Risha Rahaman <risha2031@gmail.com>
2023-03-03 16:30:03 +0530

    leetcode medium

--- src/leetcode/medium/medium_28.java
+++ src/leetcode/medium/medium_28.java
@@ -2,7 +2,7 @@ package medium;
 
 public class medium_28 {
     public static void main(String[] args) {
-        System.out.println(strStr("overflow", "flow"));
+        System.out.println(strStr("overflow", "over"));
     }
     public static int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);   
