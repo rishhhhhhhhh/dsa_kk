4601c5bedb097f13f030f0d00b06f2424069d257
Risha Rahaman <risha2031@gmail.com>
2023-03-11 13:02:29 +0530

    leetcode easy

---
+++ src/leetcode/easy/easy/easy_202.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class easy_202 {
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+        
+    }
+    public static boolean isHappy(int n) {
+        int s=0;
+        while(n>0){
+            int a = n%10;
+            s += a*a;;
+            n /=10;
+        } if(s == 1){
+            return true;
+        }else if(s == 4){
+            return false;
+        }
+        return isHappy(s);
+    }
+}
