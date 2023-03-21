02afc82c590b141148d69e58a7ce29b70205cef4
Risha Rahaman <risha2031@gmail.com>
2023-03-21 15:42:13 +0530

    leetcode easy

---
+++ src/leetcode/easy/easy/easy_1108.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class easy_1108 {
+    public static void main(String[] args) {
+        System.out.println(defangIPaddr("1.1.1.1"));
+    }
+    public static String defangIPaddr(String address) {
+        return address.replace(".","[.]");
+    }
+}
