a87f489dc54dfd8bdd8fd1e70e003913c5ff8fcd
Risha Rahaman <risha2031@gmail.com>
2023-03-22 19:13:21 +0530

    leetcode easy

---
+++ src/leetcode/easy/easy/easy_2235.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class easy_2235{
+    public static void main(String[] args) {
+        System.out.println(sum(5, -4));
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
