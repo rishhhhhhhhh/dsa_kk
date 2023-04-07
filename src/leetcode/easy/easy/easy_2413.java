e5fff02791b03da23dcb1caba0a4337f7572b75e
Risha Rahaman <risha2031@gmail.com>
2023-04-07 10:03:59 +0530

    leetcode easy

---
+++ src/leetcode/easy/easy/easy_2413.java
@@ -0,0 +1,21 @@
+package easy.easy;
+
+public class easy_2413 {
+    public static void main(String[] args) {
+        
+    }
+
+
+        public static int smallestEvenMultiple(int n) {
+    
+            if((n & 1) == 0)
+    
+                return n;
+    
+            else
+    
+                return 2*n;
+    
+        }
+    
+    }
