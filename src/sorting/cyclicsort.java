52b0acfe94499728f31308fbb4f556676dfd36f2
Risha Rahaman <risha2031@gmail.com>
2023-04-10 15:04:22 +0530

    cyclic sort

--- src/sorting/cyclicsort.java
+++ src/sorting/cyclicsort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class cyclicsort {
     public static void main(String[] args) {
-        int[] arr = {3,2,4,5,1};
+        int[] arr = {3,2,4,5,1,0};
         cyclic(arr);
         System.out.println(Arrays.toString(arr));  
     }
