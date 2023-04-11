c687c566968a1c0d93d6cbb5ef65dfb2017987f2
Risha Rahaman <risha2031@gmail.com>
2023-04-11 15:02:33 +0530

    portfolio

---
+++ src/advancedjava/q1.java
@@ -0,0 +1,37 @@
+package advancedjava;
+import java.beans.*;
+
+class Person implements java.io.Serializable {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Program5 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Jeon jungkook");
+        person.setAge(20);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
