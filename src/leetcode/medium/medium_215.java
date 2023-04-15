53f8517f4585c2b86adbf8423feb25b8fdd077fa
Risha Rahaman <risha2031@gmail.com>
2023-04-15 18:27:55 +0530

    leetcode medium

---
+++ src/leetcode/medium/medium_215.java
@@ -0,0 +1,14 @@
+package medium;
+
+import java.util.Arrays;
+
+public class medium_215 {
+    public static void main(String[] args) {
+        int[] nums={3,2,1,5,6,4};
+        System.out.println(findKthLargest(nums, 2));
+    }
+    public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+}
+}
