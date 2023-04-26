5f9e0d1ccd920376a0cc1c3994482eb625407943
Risha Rahaman <risha2031@gmail.com>
2023-04-26 17:14:57 +0530

    leetcode medium

---
+++ src/leetcode/medium/medium_48.java
@@ -0,0 +1,33 @@
+public class medium_48 {
+    public static void main(String[] args) {
+        
+    }
+
+    
+        public static void transpose(int[][] matrix){
+            for(int i = 0; i < matrix.length; i++){
+                for(int j = i; j < matrix[0].length; j++){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        
+        public void reverse(int[] arr){
+            int i = 0, j = arr.length-1;
+            while(i <= j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        
+        public void rotate(int[][] matrix) {
+            transpose(matrix);
+            for(int[] arr : matrix)
+                reverse(arr);
+        }
+    }
