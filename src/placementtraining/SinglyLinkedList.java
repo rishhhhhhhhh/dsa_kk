6986172d4ce15de964e5645a2ba0563cd1d98365
Risha Rahaman <risha2031@gmail.com>
2023-06-14 11:58:02 +0530

    linked list updated

--- src/placementtraining/SinglyLinkedList.java
+++ src/placementtraining/SinglyLinkedList.java
@@ -44,6 +44,36 @@ class LinkedList {
         System.out.println("Inserted " + data + " at the end.");
     }
 
+    void deleteAtBeginning() {
+        if (head == null) {
+            System.out.println("Linked list is empty. Nothing to delete.");
+        } else {
+            int deletedData = head.data;
+            head = head.next;
+            System.out.println("Deleted " + deletedData + " from the beginning.");
+        }
+    }
+
+    void deleteAtEnd() {
+        if (head == null) {
+            System.out.println("Linked list is empty. Nothing to delete.");
+        } else if (head.next == null) {
+            int deletedData = head.data;
+            head = null;
+            System.out.println("Deleted " + deletedData + " from the end.");
+        } else {
+            Node current = head;
+            Node previous = null;
+            while (current.next != null) {
+                previous = current;
+                current = current.next;
+            }
+            int deletedData = current.data;
+            previous.next = null;
+            System.out.println("Deleted " + deletedData + " from the end.");
+        }
+    }
+
     void insertAtPosition(int data, int position) {
         if (position <= 0) {
             System.out.println("Invalid position.");
@@ -97,9 +127,11 @@ public class SinglyLinkedList {
             System.out.println("\nLinked List Operations");
             System.out.println("1. Insert at the beginning");
             System.out.println("2. Insert at the end");
-            System.out.println("3. Insert at a specific position");
-            System.out.println("4. Display the linked list");
-            System.out.println("5. Exit");
+            System.out.println("3. Delete from the beginning");
+            System.out.println("4. Delete from the end");
+            System.out.println("5. Insert at a specific position");
+            System.out.println("6. Display the linked list");
+            System.out.println("7. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
 
@@ -115,22 +147,28 @@ public class SinglyLinkedList {
                     list.insertAtEnd(data);
                     break;
                 case 3:
+                    list.deleteAtBeginning();
+                    break;
+                case 4:
+                    list.deleteAtEnd();
+                    break;
+                case 5:
                     System.out.print("Enter the element to insert: ");
                     data = scanner.nextInt();
                     System.out.print("Enter the position: ");
                     position = scanner.nextInt();
                     list.insertAtPosition(data, position);
                     break;
-                case 4:
+                case 6:
                     list.display();
                     break;
-                case 5:
-                    System.out.println("Exiting the program");
+                case 7:
+                    System.out.println("Exiting the program...");
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-        } while (choice != 5);
+        } while (choice != 7);
 
         scanner.close();
     }
