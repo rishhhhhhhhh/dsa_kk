2480ff2689c34f8a5b1bfaa71e0c144bf795bb86
Risha Rahaman <risha2031@gmail.com>
2023-06-15 20:31:41 +0530

    leetcode medium

---
+++ src/leetcode/medium/medium_1161.java
@@ -0,0 +1,39 @@
+import java.util.ArrayDeque;
+
+class TreeNode {
+int val;
+TreeNode left;
+     TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+
+ class Solution {
+  public int maxLevelSum(TreeNode root) {
+    int max = Integer.MIN_VALUE, ans = 0;
+    var queue = new ArrayDeque<TreeNode>();
+    queue.offer(root);
+
+    for (var level = 1; !queue.isEmpty(); level++) {
+      var sum = 0;
+
+      for (var i = queue.size(); i > 0 ; i--) {
+        var node = queue.poll();
+
+        sum += node.val;
+        if (node.left != null) queue.offer(node.left);
+        if (node.right != null) queue.offer(node.right);
+      }
+      if (sum > max) {
+        max = sum;
+        ans = level;
+      }
+    }
+    return ans;
+  }
+}
