90c5e5d879d27215760750715d316dd6e973043a
Risha Rahaman <risha2031@gmail.com>
2023-06-28 10:13:36 +0530

    leetcode medium

--- src/leetcode/medium/medium_373.java
+++ src/leetcode/medium/medium_373.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> res = new ArrayList<>();
