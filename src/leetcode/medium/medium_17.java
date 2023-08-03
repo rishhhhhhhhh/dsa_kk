81bf2a7c1855e2e91b9a25ee2f434377e9222cc9
Risha Rahaman <112609390+rishhhhhhhhh@users.noreply.github.com>
2023-08-03 23:42:50 +0530

    leetcode medium

--- src/leetcode/medium/medium_17.java
+++ src/leetcode/medium/medium_17.java
@@ -1,7 +1,7 @@
 package leetcode.medium;
 
 import java.util.ArrayList;
-public class medium_17 {
+class Solution {
     public static void main(String[] args) {
         System.out.println(telephone("", "23"));
     }
