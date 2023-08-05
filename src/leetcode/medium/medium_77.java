211c4264b2ab626829dc07b6bd6bccd196c44ac1
Risha Rahaman <risha2031@gmail.com>
2023-08-05 11:55:43 +0530

    leetcode medium

--- src/leetcode/medium/medium_77.java
+++ src/leetcode/medium/medium_77.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
